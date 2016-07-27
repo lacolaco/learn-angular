cc4374d9f8a28f4daeae2cd2a22b4221b274cc50
laco0416 <laco0416+github@gmail.com>
2016-07-27 21:21:11 +0900

    add routing-1

---
+++ routing-1/README.md
@@ -0,0 +1,13 @@
+## Routing-1
+
+- RouterConfig
+  - /home
+  - /sub
+- routerLink
+- Router
+  - navigate
+  - navigateByUrl  
+- children
+  - /detail/:id
+  - ActivatedRoute
+
---
+++ routing-1/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>Routing</h1>
+  `
+})
+export class MyApp implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
